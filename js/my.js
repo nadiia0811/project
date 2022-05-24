c520445b2ea6497b61fe1eadb4345d9a8231604a
Nadya <nb1985@ukr.net>
2022-05-24 16:01:20 +0300

    Changed js file, task

--- js/my.js
+++ js/my.js
@@ -3,24 +3,41 @@
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
-  count : numberOfFilms,
+  count: numberOfFilms,
   movies: {},
   actors: {},
   genres: [],
   privat: false
 };
 
+ 
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Один из последних просмотренных фильмов?', '');
+  const b = prompt('На сколько оцените его?', '');
 
-const a = prompt('Один из последних просмотренных фильмов?', '');
-const b = prompt('На сколько оцените его?', '');
-const c = prompt('Один из последних просмотренных фильмов?', '');
-const d = prompt('На сколько оцените его?', '');
+  if(a != null && b != null && a != '' && b != '' && a.length < 50){
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+  }else{
+    console.log('error');
+    i--;
+  }
+}
+
+if(personalMovieDB.count < 10){
+  console.log('Просмотрено слишком мало фильмов');
+}else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+  console.log('Вы классический зритель');
+}else if(personalMovieDB.count >= 30){
+  console.log(' Вы киноман');
+}else{
+  console.log('error');
+}
 
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+
+
+
+
 console.log(personalMovieDB);
-
-
-
-
